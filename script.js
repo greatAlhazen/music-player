2793b14e136f3af7cef2f39532d81cb3c56212e7
Enes Uraz <axeprime91@gmail.com>
2023-05-11 20:07:25 +0300

    create timeupdate,progress click and audio ended functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// select all required elements and specify custom variable
 const audio = document.getElementById("audio");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
@@ -7,9 +8,11 @@ const artist = document.getElementById("artist");
 const image = document.getElementById("image");
 const currentTimeElement = document.getElementById("current");
 const durationElement = document.getElementById("duration");
+const progress = document.getElementById("progress");
 let isPLaying;
 let currentSong = 2;
 
+// musics array
 const musics = [
   {
     name: "music-1",
@@ -33,12 +36,14 @@ const musics = [
   },
 ];
 
+// play song, change isPlaying,title attribute and icon
 function songPlay() {
   isPLaying = true;
   play.classList.replace("fa-play", "fa-pause");
   play.setAttribute("title", "pause");
   audio.play();
 }
+// pause song, change isPlaying,title attribute and icon
 function pausePLay() {
   isPLaying = false;
   play.classList.replace("fa-pause", "fa-play");
@@ -46,8 +51,10 @@ function pausePLay() {
   audio.pause();
 }
 
+// play or pause song according to isPLaying variable
 play.addEventListener("click", () => (isPLaying ? pausePLay() : songPlay()));
 
+// change ui according to playing music
 const loadMusic = (music) => {
   title.textContent = music.showName;
   artist.textContent = music.artist;
@@ -56,8 +63,10 @@ const loadMusic = (music) => {
   durationElement.textContent = "2:02";
 };
 
+// first load
 loadMusic(musics[currentSong]);
 
+// disbale next icon
 function nextDisbale(value) {
   if (value === "disable") {
     next.style.opacity = "0.5";
@@ -68,6 +77,7 @@ function nextDisbale(value) {
   }
 }
 
+// disable prev icon
 function prevDisbale(value) {
   if (value === "disable") {
     prev.style.opacity = "0.5";
@@ -78,6 +88,7 @@ function prevDisbale(value) {
   }
 }
 
+// play next music and specify disabled part
 function nextMusic() {
   if (currentSong === 2) {
     nextDisbale("disable");
@@ -90,6 +101,7 @@ function nextMusic() {
   songPlay();
 }
 
+// pause next music and specify disabled part
 function prevMusic() {
   if (currentSong === 1) {
     prevDisbale("disable");
@@ -102,5 +114,63 @@ function prevMusic() {
   songPlay();
 }
 
+// next and prev click event
 next.addEventListener("click", nextMusic);
 prev.addEventListener("click", prevMusic);
+
+//
+function timeUpdate(e) {
+  if (isPLaying) {
+    // get and specify time music is in
+    const { duration, currentTime } = e.srcElement;
+    const time = (currentTime / duration) * 100;
+    progress.style.setProperty("--width", time);
+    // get and specify duration
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSecond = Math.floor(duration % 60);
+    if (durationSecond < 10) {
+      durationSecond = `0${durationSecond}`;
+    }
+    if (durationSecond) {
+      durationElement.textContent = `${durationMinutes}:${durationSecond}`;
+    }
+
+    // get and specify current time
+    const currentTimeMinutes = Math.floor(currentTime / 60);
+    let currentTimeSecond = Math.floor(currentTime % 60);
+    if (currentTimeSecond < 10) {
+      currentTimeSecond = `0${currentTimeSecond}`;
+    }
+    if (currentTimeSecond) {
+      currentTimeElement.textContent = `${currentTimeMinutes}:${currentTimeSecond}`;
+    }
+  }
+}
+// time update event
+audio.addEventListener("timeupdate", timeUpdate);
+
+// progress click functionlity
+function progressClick(e) {
+  const clickX = e.offsetX;
+  const width = this.clientWidth;
+  audio.currentTime = (clickX / width) * audio.duration;
+}
+
+// progress click event
+progress.addEventListener("click", progressClick);
+
+// ended music functionality
+function endedMusic() {
+  if (currentSong === 3) {
+    currentSong = 0;
+    prevDisbale("disable");
+    nextDisbale("notDisable");
+    loadMusic(musics[currentSong]);
+    songPlay();
+  } else {
+    nextMusic();
+  }
+}
+
+// ended event
+audio.addEventListener("ended", endedMusic);
